1541c75fa9ea4ba1f0e1625ffb4acd05fe1c60aa
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-08-04 05:08:04 +0100

    update

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,27 @@
 """filtered logger module"""
 from typing import List
 import re
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """format a record"""
+        filtered = filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)  # nopep8
+        log_record = logging.LogRecord("my_logger", logging.INFO, None, None, re.sub(';', '; ', filtered).strip(), None, None)  # nopep8
+        formatter = logging.Formatter(self.FORMAT)
+        return formatter.format(log_record)
 
 
 def filter_datum(fields: List, redaction: str, message: str, separator: str) -> str:  # nopep8
