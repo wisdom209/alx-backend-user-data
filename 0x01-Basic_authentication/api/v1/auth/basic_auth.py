87820c45efb9816997b7db25910ed684be131809
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-08-08 20:01:46 +0100

    validate password function

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 """Basic authentication module"""
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -41,3 +43,18 @@ class BasicAuth(Auth):
         email = decoded_base64_authorization_header.split(':')[0]
         password = decoded_base64_authorization_header.split(':')[1]
         return (email, password)
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):  # nopep8
+        """Returns user instance"""
+        if user_email is None or type(user_email) is not str:
+            return None
+        if user_pwd is None or type(user_pwd) is not str:
+            return None
+        user = User.search({'email': user_email})
+        if not user or len(user) == 0 or not user[0]:
+            return None
+        user = user[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+        else:
+            return user
