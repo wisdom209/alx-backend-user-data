621806318b12a2427dfc7e96a226777159d2bce4
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-08-10 15:14:51 +0100

    update

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Session auth class"""
 from api.v1.auth.auth import Auth
+from models.user import User
 import uuid
 
 
@@ -24,3 +25,9 @@ class SessionAuth(Auth):
         if type(session_id) is not str:
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """returns a User instance based on a cookie value:"""
+        user_id = self.user_id_for_session_id(self.session_cookie(request))  # nopep8
+        user = User.get(user_id)
+        return user
