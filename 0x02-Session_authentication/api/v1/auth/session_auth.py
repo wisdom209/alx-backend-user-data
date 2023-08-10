0a76a2b4c0951f2be69a1229ddc534f4baf91c6c
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-08-10 14:08:31 +0100

    update

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 """Session auth class"""
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """Session auth class"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """create a session id"""
+        if user_id is None or type(user_id) is not str:
+            return None
+        else:
+            session_id = str(uuid.uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
