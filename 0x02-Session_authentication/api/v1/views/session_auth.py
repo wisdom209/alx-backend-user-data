be820595f923318426cf8e9692ce62f0b93cd42a
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-08-10 15:54:01 +0100

    update

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""session auth routes module"""
+
+from api.v1.views import app_views
+from flask import abort, jsonify, request
+from models.user import User
+import os
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def session_login() -> str:
+    """ POST SESSION DETAILS
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not email or email == '':
+        return jsonify({"error": "email missing"}), 400
+
+    if not password or password == '':
+        return jsonify({"error": "password is missing"}), 400
+
+    user = User.search({"email": email})
+
+    if (not user or len(user) == 0):
+        return jsonify({"error": "no user found for this email"}), 404
+
+    user = user[0]
+
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+
+    from api.v1.app import auth
+
+    session_id = auth.create_session(user.id)
+    res = jsonify(user.to_json())
+    session_name = os.getenv('SESSION_NAME')
+    res.set_cookie(session_name, session_id)
+    return res
