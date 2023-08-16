9fe5e987ffb8ddaad4dc352bdf44ca80eb7a071b
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-08-16 14:57:09 +0100

    add user to db

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -12,7 +12,7 @@ Base = declarative_base()
 
 
 class User(Base):
-    """User mode class"""
+    """User model class"""
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
