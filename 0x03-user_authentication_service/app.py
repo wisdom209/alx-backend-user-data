7af3f49801bc8184995e811307b4fd3ce48e9f59
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-08-17 01:19:24 +0100

    setup a basic flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Basic flask app"""
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+
+@app.route('/')
+def welcome():
+    """welcome route"""
+    return jsonify({"message": "Bienvenue"}), 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
