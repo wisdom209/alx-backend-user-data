60f94befa3fb3e497577707d07d40d6ccb8dad31
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-08-17 02:15:57 +0100

    update session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -37,6 +37,16 @@ class Auth:
         except NoResultFound:
             return False
 
+    def create_session(self, email: str) -> str:
+        """create a session id"""
+        try:
+            user = self._db.find_user_by(email=email)
+            user.session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=user.session_id)
+            return user.session_id
+        except NoResultFound:
+            pass
+
 
 def _hash_password(password: str) -> bytes:
     """hash password"""
