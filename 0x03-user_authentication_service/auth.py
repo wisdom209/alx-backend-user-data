cfc6d6d4d32fffde1f1ebcc108b00b07913a2f07
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-08-19 01:37:39 +0100

    update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -79,6 +79,7 @@ class Auth:
             raise ValueError
 
     def update_password(self, reset_token: str, password: str) -> None:
+        """updates user password"""
         try:
             user = self._db.find_user_by(reset_token=reset_token)
             pass_hash = _hash_password(password)
