e4f5c0310c57aacfa204e85c70bfdd4cf5eb859e
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-08-19 06:59:14 +0100

    update

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -88,7 +88,7 @@ def get_reset_password_token():
 
 
 @app.route('/reset_password', methods=['PUT'], strict_slashes=False)
-def update_password():
+def update_password() -> Response:
     """get reset password token"""
     try:
         email = request.form.get('email')
